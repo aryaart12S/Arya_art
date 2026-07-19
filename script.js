680c17290c6af6aad1690fae7f95555e47704d3d
aryaart12S <swayamnagwani07@gmail.com>
2026-07-19 17:02:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,25 @@ document.querySelectorAll(".gallery img").forEach(img=>{
     });
 
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+
+let slides=document.getElementsByClassName("slides");
+
+for(let i=0;i<slides.length;i++){
+slides[i].style.display="none";
+}
+
+slideIndex++;
+
+if(slideIndex>slides.length){
+slideIndex=1;
+}
+
+slides[slideIndex-1].style.display="block";
+
+setTimeout(showSlides,3000);
+
+}
