7d1343d1872fae76a2970ad8b40c0f22e2ad32ee
aryaart12S <swayamnagwani07@gmail.com>
2026-07-25 17:20:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,26 +13,7 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     });
 });
 
-// Fade-in animation
-const sections = document.querySelectorAll("section");
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.style.opacity="1";
-            entry.target.style.transform="translateY(0)";
-        }
-    });
-},{
-    threshold:0.15
-});
-
-sections.forEach(section=>{
-    section.style.opacity="0";
-    section.style.transform="translateY(40px)";
-    section.style.transition="all 0.8s ease";
-    observer.observe(section);
-});
 
 // Click image to view larger
 document.querySelectorAll(".gallery img").forEach(img=>{
