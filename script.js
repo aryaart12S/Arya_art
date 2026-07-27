8deb6ffc3db040482e91b0517dd2cb7bd37018c6
aryaart12S <swayamnagwani07@gmail.com>
2026-07-27 17:57:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,23 @@ const observer = new IntersectionObserver((entries)=>{
 elements.forEach(element=>{
     observer.observe(element);
 });
+
+function searchProducts(){
+
+let input=document.getElementById("searchInput").value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let title=card.querySelector("h3").innerText.toLowerCase();
+
+if(title.includes(input)){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+}
